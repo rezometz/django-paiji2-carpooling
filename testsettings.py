f6bb35a433ce8e7b829953fc3e14e46ee0a24386
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-08-06 13:02:01 +0200

    Add test setup

---
+++ testsettings.py
@@ -0,0 +1,24 @@
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+
+SECRET_KEY = 'tsktsktsk'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    # Default Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # We test this one
+    'paiji2_carpooling',
+)
