558f4ae9d600df327f07fe8413fe477dbef3d02e
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-25 22:02:21 +0200

    corrects url inclusion in modular.py

--- paiji2_carpooling/modular.py
+++ paiji2_carpooling/modular.py
@@ -2,11 +2,13 @@ from django.conf.urls import url, include
 
 from modular_blocks import ModuleApp, TemplateTagBlock, modules
 
+from . import urls
+
 
 class CarpoolingModule(ModuleApp):
     app_name = 'cov'
     name = 'cov'
-    urls = url(r'^cov/', include('cov.urls'))
+    urls = url(r'^carpooling/', include(urls))
     templatetag_blocks = [
         TemplateTagBlock(
             name='cov',
